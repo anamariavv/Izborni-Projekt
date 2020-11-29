5cf0af1d75c34e2d562dcd72a507e7a7270ba216
Anamaria <35101063+anamariavv@users.noreply.github.com>
2020-11-29 16:10:49 +0100

    updated file
    added collation selection after connection

--- include/database_connect.inc.php
+++ include/database_connect.inc.php
@@ -3,11 +3,15 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "database"; 
+    $database = "database";
     $port = 3308; //adjust this based your amp->default is usually 3306
 
     $conn = mysqli_connect($servername, $username, $password, $database, $port);
-
+   
+    if (!$conn->set_charset("utf8")) {
+        printf("Error loading character set utf8: %s\n", $conn->error);
+        exit();
+    }
     if(!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
