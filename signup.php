1ad8fa2ad685b7553c2cc3c6d3ac67cf3d087ae0
Anamaria <35101063+anamariavv@users.noreply.github.com>
2020-12-17 14:51:31 +0100

    Updated files
    added a reference to corresponding javascript files for each signup page

--- signup.php
+++ signup.php
@@ -4,10 +4,10 @@
 
 <h2>Sign up</h2>
 
-<form action="include/signup.inc.php" method="post" target="_self">
+<form action="include/signup.inc.php" method="post" target="_self" name="signupform">
     Firstname: <input type="text" id="firstname" name="firstname"><br>
     Lastname: <input type="text" id="lastname" name="lastname"><br>
-    OIB: <input type="number" id="oib" name="oib"><br>
+    OIB: <input type="number" id="oib" name="oib"><br> <!--nije oib na engleskom->dodaj pored polja-->
     Age: <input type="number" id="age" name="age"><br>
     E-mail: <input type="email" id="email" name="email"><br>
     City: <input type="text" id="city" name="city"><br> 
@@ -17,23 +17,16 @@
     <input type="submit" value="Sign up!" name="signupsubmit">  
 </form>
 
+<script src="js/signup.js"></script> 
+
 <?php
     if(isset($_GET["error"])) {
         if($_GET["error"] == "sql") {
             echo "<p>There was an SQL error. Please try again.</p>";
         }
-        if($_GET["error"] == "empty_fields") {
-            echo "<p>Please fill in all fields.</p>";
-        }
         if($_GET["error"] == "email_taken") {
-            echo "<p>This email is already taken.</p>";
+            echo "<p>This email is already in use.</p>";
         }
-        if($_GET["error"] == "password_weak") {
-            echo "<p>The password must be 8-32 characters long, with atlease one uppercase letter and number.</p>";
-        }
-        if($_GET["error"] == "password_mismatch") {
-            echo "<p>Passwords don't match.</p>";
-        } 
     }
 
     include_once "footer.php";
--- signup_company.php
+++ signup_company.php
@@ -4,7 +4,7 @@
 
     <h2>Partner with us!</h2>
 
-<form action="include/signup_company.inc.php" method="post" target="_self">
+<form action="include/signup_company.inc.php" method="post" target="_self" name="c_signupform">
     Company name: <input type="text" id="cname" name="cname"><br>
     City: <input type="text" id="city" name="city"><br>
     Address: <input type="text" id="address" name="address"><br>
@@ -17,23 +17,16 @@
     <input type="submit" value="Sign up!" name="signup_companysubmit">  
 </form>
 
+<script src="js/signup_company.js"></script>
+
 <?php
     if(isset($_GET["error"])) {
         if($_GET["error"] == "sql") {
             echo "<p>There was an SQL error. Please try again.</p>";
         }
-        if($_GET["error"] == "empty_fields") {
-            echo "<p>Please fill in all fields.</p>";
-        }
         if($_GET["error"] == "email_taken") {
             echo "<p>This email is already taken.</p>";
         }
-        if($_GET["error"] == "password_weak") {
-            echo "<p>The password must be 8-32 characters long, with atleast one uppercase letter and number.</p>";
-        }
-        if($_GET["error"] == "password_mismatch") {
-            echo "<p>Passwords don't match.</p>";
-        } 
     }
 
     include_once "footer.php";
