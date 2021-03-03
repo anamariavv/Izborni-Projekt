e9474f85d832dd7242e8ce41c7a6f979d29cd9a3
Anamaria <35101063+anamariavv@users.noreply.github.com>
2021-03-03 21:04:36 +0100

    updated edit_resume.inc.php

--- include/edit_resume.inc.php
+++ include/edit_resume.inc.php
@@ -24,7 +24,6 @@
     if(isset($_POST['education'])) {
         //loop through array->for each row, turn it into sql and update in database
 
-
         foreach($_POST['education'] as $row) {
             $start_year = mysqli_real_escape_string($conn, $row['start_year']);
             $end_year = mysqli_real_escape_string($conn, $row['end_year']);
@@ -45,6 +44,101 @@
                 $message = "SQL error";
             }
         } 
+    }
+
+    if(isset($_POST['skills'])) {
+
+        foreach ($_POST['skills'] as $row) {
+           $id = mysqli_real_escape_string($conn, $row['id']);
+           $name = mysqli_real_escape_string($conn, $row['name']);
+           $level = mysqli_real_escape_string($conn, $row['level']);
+
+           $sql = 'UPDATE skill SET name = ?, level = ? WHERE id = ?';
+            
+            
+            if(!($stmt = $conn->prepare($sql))) {
+                $message = "SQL error";
+            }
+            if(!($stmt->bind_param("sis", $category, $level, $id))) {
+                $message = "SQL error";
+            }
+            if(!($stmt->execute())) {
+                $message = "SQL error";
+            }
+
+        }
+    }
+
+    if(isset($_POST['languages'])) {
+
+        foreach ($_POST['languages'] as $row) {
+           $id = mysqli_real_escape_string($conn, $row['id']);
+           $name = mysqli_real_escape_string($conn, $row['name']);
+           $level = mysqli_real_escape_string($conn, $row['level']);
+
+           $sql = 'UPDATE language SET name = ?, level = ? WHERE id = ?';
+            
+            
+            if(!($stmt = $conn->prepare($sql))) {
+                $message = "SQL error";
+            }
+            if(!($stmt->bind_param("sis", $name, $level, $id))) {
+                $message = "SQL error";
+            }
+            if(!($stmt->execute())) {
+                $message = "SQL error";
+            }
+
+        }
+    }
+
+    if(isset($_POST['keywords'])) {
+
+        foreach ($_POST['keywords'] as $row) {
+           $id = mysqli_real_escape_string($conn, $row['id']);
+           $category = mysqli_real_escape_string($conn, $row['category']);
+           $word = mysqli_real_escape_string($conn, $row['word']);
+
+           $sql = 'UPDATE keyword SET category = ?, word = ? WHERE id = ?';
+            
+            if(!($stmt = $conn->prepare($sql))) {
+                $message = "SQL error";
+            }
+            if(!($stmt->bind_param("sss", $category, $word, $id))) {
+                $message = "SQL error";
+            }
+            if(!($stmt->execute())) {
+                $message = "SQL error";
+            }
+        }
+    }
+
+    if(isset($_POST['work'])) {
+
+        foreach ($_POST['work'] as $row) {
+           $id = mysqli_real_escape_string($conn, $row['id']);
+           $start_month = mysqli_real_escape_string($conn, $row['start_month']);
+           $start_year = mysqli_real_escape_string($conn, $row['start_year']);
+           $end_month = mysqli_real_escape_string($conn, $row['end_month']);
+           $end_year = mysqli_real_escape_string($conn, $row['end_year']);
+           $title = mysqli_real_escape_string($conn, $row['title']);
+           $city = mysqli_real_escape_string($conn, $row['city']);
+           $country = mysqli_real_escape_string($conn, $row['country']);
+           $description = mysqli_real_escape_string($conn, $row['description']);
+           
+
+           $sql = 'UPDATE work_experience SET start_month = ?, end_month = ?, start_year = ?, end_year = ?, title = ?, country = ?, city = ?, description = ? WHERE id = ?';
+            
+            if(!($stmt = $conn->prepare($sql))) {
+                $message = "SQL error";
+            }
+            if(!($stmt->bind_param("iiiissssi", $start_month, $end_month, $start_year, $end_year, $title, $country, $city, $description, $id))) {
+                $message = "SQL error";
+            }
+            if(!($stmt->execute())) {
+                $message = "SQL error";
+            }
+        }
     }
     echo json_encode($message);
 
