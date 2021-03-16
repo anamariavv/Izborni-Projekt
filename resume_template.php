497edd64b9b449f9ce16920952484e3dc0fa143f
Anamaria <35101063+anamariavv@users.noreply.github.com>
2021-03-16 16:24:00 +0100

    Fixed a bug
    Removed edit button when there are no elements for a resume sector, fixed a bug where education adding was appended to skill div

--- resume_template.php
+++ resume_template.php
@@ -32,12 +32,14 @@
             echo '<tr><td hidden id="education_id'.$count.'">'.$row_education['id'].'</td><td id="education_start_year'.$count.'">'.$row_education['start_year'].'</td><td id="education_end_year'.$count.'">'.$row_education['end_year'].'</td><td id="education_title'.$count.'">'.$row_education['title'].'</td><td id="education_country'.$count.'">'.$row_education['country'].'</td><td id="education_city'.$count.'">'.$row_education['city'].'</td></tr>';
             $count += 1;
         }
-    echo '</tbody></table></div>
-        <div id="edit_education_button">';
-        if($_SESSION['user_level'] == "student") {
-            echo "<button type='button' id='edit_education'>Edit</button>";
+    echo '</tbody></table></div>';
+        if(!empty($array_education)){ 
+            echo '<div id="edit_education_button">';
+            if($_SESSION['user_level'] == "student") {
+                echo "<button type='button' id='edit_education'>Edit</button>";
+            }
+            echo '</div>';   
         }
-    echo '</div>';
     echo '<div id="add_education_button">';
         if($_SESSION['user_level'] == "student") {
             echo "<button type='button' id='add_education'>Add</button>";
@@ -46,19 +48,22 @@
 
     echo '<h2>Work Experience</h2>
         <div id="work_experience">
-        <table id="work_table" width="35%"';
+        <table id="work_table" width="35%">';
         $count = 1;
         foreach($array_work as $row_work) {
             echo '<tr><th hidden id="work_id'.$count.'">'.$row_work['id'].'</th><th id="work_start_month'.$count.'">'.$row_work['start_month'].'</th><th id="work_start_year'.$count.'">'.$row_work['start_year'].'</th><th id="work_end_month'.$count.'">'.$row_work['end_month'].'</th><th id="work_end_year'.$count.'">'.$row_work['end_year'].'</th><th id="work_title'.$count.'">'.$row_work['title'].'</th><th id="work_city'.$count.'">'.$row_work['city'].'</th><th id="work_country'.$count.'">'.$row_work['country'].'</th></tr>';
             echo '<tr><td colspan="7" id="work_description'.$count.'">'.$row_work['description'].'</td></tr>';
             $count += 1;
         }
-    echo '</table></div>
-        <div id="edit_work_experience_button">';
-        if($_SESSION['user_level'] == "student") {
-            echo "<button type='button' id='edit_work_experience'>Edit</button>";
+    echo '</table></div>';
+        if(!empty($array_work)) {
+            echo '<div id="edit_work_experience_button">';
+            if($_SESSION['user_level'] == "student") {
+                echo "<button type='button' id='edit_work_experience'>Edit</button>";
+            }
+            echo '</div>';
         }
-    echo '</div>';
+        
     echo '<div id="add_work_button">';
         if($_SESSION['user_level'] == "student") {
             echo "<button type='button' id='add_work'>Add</button>";
@@ -74,12 +79,14 @@
             echo '<tr><td hidden id="skill_id'.$count.'">'.$row_skill['id'].'</td><td id="skill_name'.$count.'">'.$row_skill['name'].'</td><td id="skill_level'.$count.'">'.$row_skill['level'].'</td></tr>';
             $count += 1;
         }
-    echo '</tbody></table></div>
-        <div id="edit_skills_button">';
-        if($_SESSION['user_level'] == "student") {
-            echo "<button type='button' id='edit_skills'>Edit</button>";
-        }
-    echo '</div>';
+    echo '</tbody></table></div>';
+        if(!empty($array_skill)) {
+            echo '<div id="edit_skills_button">';
+            if($_SESSION['user_level'] == "student") {
+                echo "<button type='button' id='edit_skills'>Edit</button>";
+            }
+            echo '</div>';
+        }  
     echo '<div id="add_skill_button">';
         if($_SESSION['user_level'] == "student") {
             echo "<button type='button' id='add_skill'>Add</button>";
@@ -95,12 +102,14 @@
             echo '<tr><td hidden id="language_id'.$count.'">'.$row_language['id'].'</td><td id="language_name'.$count.'">'.$row_language['name'].'</td><td id="language_level'.$count.'">'.$row_language['level'].'</td></tr>';
             $count += 1;
         }        
-    echo '</tbody></table></div>
-        <div id="edit_languages_button">';
-        if($_SESSION['user_level'] == "student") {
-            echo "<button type='button' id='edit_languages'>Edit</button>";
+    echo '</tbody></table></div>';
+        if(!empty($array_language)) {
+            echo  '<div id="edit_languages_button">';
+            if($_SESSION['user_level'] == "student") {
+                echo "<button type='button' id='edit_languages'>Edit</button>";
+            }
+            echo '</div>';
         }
-    echo '</div>';
     echo '<div id="add_language_button">';
         if($_SESSION['user_level'] == "student") {
             echo "<button type='button' id='add_language'>Add</button>";
@@ -116,12 +125,14 @@
             echo '<tr><td hidden id="keyword_id'.$count.'">'.$row_keyword['id'].'</td><td id="keyword_category'.$count.'">'.$row_keyword['category'].'</td><td id="keyword_word'.$count.'">'.$row_keyword['word'].'</td></tr>';
             $count += 1;
         }
-    echo '</tbody></table></div>
-        <div id="edit_keywords_button">';
-        if($_SESSION['user_level'] == "student") {
-            echo "<button type='button' id='edit_keywords'>Edit</button>";
+    echo '</tbody></table></div>';
+        if(!empty($array_keyword)) {
+            echo '<div id="edit_keywords_button">';
+            if($_SESSION['user_level'] == "student") {
+                echo "<button type='button' id='edit_keywords'>Edit</button>";
+            }
+            echo '</div>';
         }
-    echo '</div>';
     echo '<div id="add_keyword_button">';
         if($_SESSION['user_level'] == "student") {
             echo "<button type='button' id='add_keyword'>Add</button>";
