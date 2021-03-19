abeabd2f397dcf4e6ce109915e525699b54ce2db
Anamaria <35101063+anamariavv@users.noreply.github.com>
2021-03-19 21:40:58 +0100

    updated file

--- include/user_list.inc.php
+++ include/user_list.inc.php
@@ -2,14 +2,38 @@
 
     require_once "database_connect.inc.php";
     
-    $sql_admin = "SELECT * FROM admin";
-    $result = $conn->query($sql_admin);
-    $administrators = $result->fetch_all(MYSQLI_ASSOC);
+    if(isset($_POST['messagesubmit'])) {
 
-    $sql_student = "SELECT * FROM student";
-    $result = $conn->query($sql_student);
-    $students = $result->fetch_all(MYSQLI_ASSOC);
+        $message = mysqli_real_escape_string($conn, $_POST['message_text']);
+        
+        if($_POST['type'] == 'student') {
+            $column = "student_oib";
+            $data = mysqli_real_escape_string($conn, $_POST['identification']);
+        } else {
+            $column = "company_id";
+            $data = "'".mysqli_real_escape_string($conn, $_POST['identification'])."'";
+        }
 
-    $sql_company = "SELECT * FROM company";
-    $result = $conn->query($sql_company);
-    $companies = $result->fetch_all(MYSQLI_ASSOC);
+        $sql = "INSERT INTO notification(notif_text,".$column.") VALUES ('". $message ."', ". $data.")";
+
+        if(!($conn->query($sql))) {
+            header("Location: ../send_notification.php?error=sql");
+            exit();
+        } else {
+            header("Location: ../user_list.php?message=sent");
+            exit();
+        }
+    
+    } else {
+        $sql_admin = "SELECT * FROM admin";
+        $result = $conn->query($sql_admin);
+        $administrators = $result->fetch_all(MYSQLI_ASSOC);
+
+        $sql_student = "SELECT * FROM student";
+        $result = $conn->query($sql_student);
+        $students = $result->fetch_all(MYSQLI_ASSOC);
+
+        $sql_company = "SELECT * FROM company";
+        $result = $conn->query($sql_company);
+        $companies = $result->fetch_all(MYSQLI_ASSOC);
+    }
