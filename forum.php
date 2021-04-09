c417d437fdc1818bdead10bd1448c40d2c1099f8
Anamaria <35101063+anamariavv@users.noreply.github.com>
2021-04-09 20:45:44 +0200

    added css

--- forum.php
+++ forum.php
@@ -3,47 +3,40 @@
     require_once "include/forum.inc.php";
     echo "<script src='js/forum_comments.js'></script>";
 
+    echo "<h1 class='h1'>".$_GET['name']." internship discussion</h1>";
 
-    echo "<h1>".$_GET['name']." internship discussion</h1>";
-    echo "<div><h3>Welcome to company forums!</h3></div>";
-    echo "<div><p>This forum is meant for students to share their internship experience for this company.
-        Please be respectful of one another. Comments will appear once approved by an administrator - inappropriate comments will not be approved or diplayed.</p></div>";
-
-    foreach($review_array as $review) {
-        if($review['status'] == 'approved') {
-            echo "<span>";
-            if($review['picture']) {
-                echo "<span><img src='".$review['picture']."' width='40' height='40'></img></span>";
-            }
-            echo "<input type='hidden' id='".$review['oib']."' value='".$review['id']."'></input><span>".$review['firstname']." ".$review['lastname']." ".$review['time']."</span></br><div id='text_".$review['id']."'>".$review['text']."</div><br></span>";
-            if($_SESSION['user_level'] == 'student') {
-                if($review['oib'] == $_SESSION['oib']) {
-                    echo "<button type='button' name='delete_comment' value='".$review['oib']."'>Delete</button>";
-                    echo "<button type='button' name='edit_comment' value='".$review['id']."'>Edit</button>";
-                }
-            }
-            echo "</br>";
-        } else if ($review['status'] == 'pending') {
-            if(($_SESSION['user_level'] == 'student' && $_SESSION['oib'] == $review['oib']) || ($_SESSION['user_level'] == 'administrator')) {
-                echo "<span>";
-                if($review['picture']) {
-                    echo "<span><img src='".$review['picture']."' width='40' height='40'></img></span>";
-                }
-                echo "<input type='hidden' id='".$review['oib']."' value='".$review['id']."'></input><span>".$review['firstname']." ".$review['lastname']." ".$review['time']." -Comment pending approval </span></br>".$review['text']."<br></span>";
-                echo "</br>";
-            }
-        } 
-    }   
-
-    //napravi form za novi komentar
     if($_SESSION['user_level'] == 'student') {
-        echo "<form action='include/new_comment.inc.php' method='post' name='new_comment_form' id='new_comment_form'>
-        <textarea form='new_comment_form' id='comment_text' name='comment_text' cols='50'></textarea><br>
+        echo "<form class='comment_form' action='include/new_comment.inc.php' method='post' name='new_comment_form' id='new_comment_form'>
+        <textarea form='new_comment_form' id='comment_text' name='comment_text' cols='60' rows='5'></textarea><br>
         <input type='hidden' name='company' value='".$_GET['company']."'></input>
         <input type='hidden' name='company_name' value='".$_GET['name']."'></input>
         <input type='submit' value='Comment' id='comment_submit'></input>
         </form>";
     }    
-     
+
+    foreach($review_array as $review) {
+        if($review['status'] == 'approved') {
+            echo "<div class='comment'>";
+            if($review['picture']) {
+                echo "<div class='comment_left'><img src='".$review['picture']."' width='40' height='40'></img></div>";
+            }
+            echo "<div class='comment_text' id='text_".$review['id']."'>".$review['text']."</div><br><input type='hidden' id='".$review['oib']."' value='".$review['id']."'></input><span class='comment_author'>".$review['firstname']." ".$review['lastname']." ".$review['time']."</span></br>";
+            if($_SESSION['user_level'] == 'student') {
+                if($review['oib'] == $_SESSION['oib']) {
+                    echo "<div class='forum_buttons'><button type='button' name='delete_comment' value='".$review['oib']."'>Delete</button>";
+                    echo "<button type='button' name='edit_comment' value='".$review['id']."'>Edit</button></div>";
+                }
+            }
+            echo "</div>";
+        } else if ($review['status'] == 'pending') {
+            if(($_SESSION['user_level'] == 'student' && $_SESSION['oib'] == $review['oib']) || ($_SESSION['user_level'] == 'administrator')) {
+                echo "<div class='comment_pending'>";
+                if($review['picture']) {
+                    echo "<div class='comment_left_pending'><img src='".$review['picture']."' width='40' height='40'></img></div>";
+                }
+                echo "<input type='hidden' id='".$review['oib']."' value='".$review['id']."'></input><span class='comment_author_pending'>".$review['firstname']." ".$review['lastname']." ".$review['time']." -Comment pending approval </span></br>".$review['text']."<br></div>";
+            }
+        } 
+    }        
     include_once "footer.php";
 ?>
